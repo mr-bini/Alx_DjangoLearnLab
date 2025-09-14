2b23ada6efb22d2e828a6c7579189032df6d771f
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-09-14 23:10:02 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,1 +1,19 @@
-# admin config placeholder
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    search_fields = ("username", "email")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
