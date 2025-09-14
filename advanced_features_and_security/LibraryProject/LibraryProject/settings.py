e58f8b19da3cfe3d0e2c909f5178733c5ffea040
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-09-14 23:12:54 +0300

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,1 +1,16 @@
+# SECURITY CONFIGURATIONS
+
+# Never keep DEBUG=True in production
+DEBUG = False  
+
+# Security middleware options
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Cookies should only be sent over HTTPS
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# Use the custom user model
 AUTH_USER_MODEL = "bookshelf.CustomUser"
