6cba8211bbbd4e8bbf7e5c586325f96a063c953b
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-09-14 23:16:14 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,8 @@
-# forms placeholder
+from django import forms
+from .models import Book
+
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author", "published_date"]
