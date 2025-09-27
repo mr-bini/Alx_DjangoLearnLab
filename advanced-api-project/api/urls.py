2b4ff35f8d186e39038e2411c945633b1acaa322
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-09-27 22:10:33 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),          # Create
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # Update
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # Delete
+    path('books/update', BookUpdateWithoutIDView.as_view(), name='book-update'),
+path('books/delete', BookDeleteWithoutIDView.as_view(), name='book-delete'),
+
 ]
