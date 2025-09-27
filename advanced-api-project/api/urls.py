c1f90c530964f633a3b6da86f7a30ccceef943dc
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-09-27 22:02:03 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,6 +6,7 @@ from .views import (
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),              # List all books
+        path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # Retrieve a book
     path('books/create/', BookCreateView.as_view(), name='book-create'),   # Create a book
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # Update a book
